c39b84cfacb09a7a9b317dc2b63d82462e4f5d6e
Steve Li <Steve.Li@monitorerp.com>
2023-04-21 10:35:43 +0800

    1

--- QueryBuilder.Tests/AggregateTests.cs
+++ QueryBuilder.Tests/AggregateTests.cs
@@ -17,7 +17,6 @@ namespace SqlKata.Tests
             Assert.Equal("SELECT COUNT(*) AS `count` FROM `A`", c[EngineCodes.MySql]);
             Assert.Equal("SELECT COUNT(*) AS \"count\" FROM \"A\"", c[EngineCodes.PostgreSql]);
             Assert.Equal("SELECT COUNT(*) AS \"COUNT\" FROM \"A\"", c[EngineCodes.Firebird]);
-            Assert.Equal("SELECT COUNT(*) AS \"count\" FROM \"A\"", c[EngineCodes.SqlAnywhere]);
         }
 
         [Fact]
